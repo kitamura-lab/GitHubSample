bc45865481c207767f7b00b602361c155c8c0e65
kitamura-lab <ykitamura@kwansei.ac.jp>
2026-02-15 09:16:11 +0900

    READMEの修正

--- pose.py
+++ pose.py
@@ -2,6 +2,6 @@ from ultralytics import YOLO
 
 model = YOLO("yolov8x-pose.pt")
 
-results = model("https://ultralytics.com/images/bus.jpg", save=True, save_txt=True, save_conf=True)
+results = model("bus.jpg")
 keypoints = results[0].keypoints
 print(keypoints.data)
